507b916bb004a5d0345ae223cbd87b5fc21b77c0
Christoph Decker <C.Decker@metalldeutsch.com>
2024-10-12 05:18:52 +0200

    j

--- src/chdScoring.App/MauiProgram.cs
+++ src/chdScoring.App/MauiProgram.cs
@@ -33,31 +33,27 @@ namespace chdScoring.App
             var directoryPath = string.Empty;
 #if ANDROID
             var docDirectory = Android.App.Application.Context.GetExternalFilesDir(Android.OS.Environment.DirectoryDocuments);
-            var fullPath = $"{docDirectory.AbsoluteFile.Path}/chdScoring/{fileName}";
+            var fullPath = $"{docDirectory.AbsoluteFile.Path}/{fileName}";
 #endif
 #if WINDOWS
             var fullPath= Path.Combine( "c:\\chdScoring\\", fileName);
 #endif
-            if (File.Exists(fullPath))
+            if (!File.Exists(fullPath))
             {
-                using Stream stream = File.OpenRead(fullPath);
-                return new ConfigurationBuilder()
-                        .AddJsonStream(stream)
-                        .Build();
-            }
-
-            var appSettingsFileName = "chdScoring.App.appsettings.json";
-            var assembly = Assembly.GetExecutingAssembly();
-            using (Stream stream = assembly.GetManifestResourceStream(appSettingsFileName))
-            {
-                if (stream == null)
+                var fs = File.Create(fullPath);
+                var appSettingsFileName = "chdScoring.App.appsettings.json";
+                var assembly = Assembly.GetExecutingAssembly();
+                using var resStream = assembly.GetManifestResourceStream(appSettingsFileName);
+                if (resStream == null)
                 {
                     throw new ApplicationException($"Unable to read file [{appSettingsFileName}]");
                 }
-                return new ConfigurationBuilder()
-                        .AddJsonStream(stream)
-                        .Build();
+                resStream.CopyTo(fs);
             }
+            using Stream stream = File.OpenRead(fullPath);
+            return new ConfigurationBuilder()
+                    .AddJsonStream(stream)
+                    .Build();
         }
 
         private static void AddServices(this MauiAppBuilder builder)
