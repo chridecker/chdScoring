a07c2468acf7b66bceaf1bbdca2b734458004b64
Christoph Decker <C.Decker@metalldeutsch.com>
2024-11-27 07:18:59 +0100

    Remove PrintService

--- src/chdScoring.PrintService/Constants/EndpointConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace chdScoring.PrintService.Constants
-{
-    public class EndpointConstants
-    {
-        public const string ROUTE_Test = "Test";
-
-        public const string ROUTE_ADD_PRINT = "Add";
-    }
-}
