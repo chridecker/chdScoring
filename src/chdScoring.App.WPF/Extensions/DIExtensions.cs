4789b868b2f38e79277c8c70c2c4b0eda4dadd07
Christoph Decker <C.Decker@metalldeutsch.com>
2025-07-09 08:05:16 +0200

    d

--- src/chdScoring.App.WPF/Extensions/DIExtensions.cs
+++ src/chdScoring.App.WPF/Extensions/DIExtensions.cs
@@ -1,17 +1,18 @@
-﻿using chdScoring.App.UI.Extensions;
+﻿using chdScoring.App.UI.Constants;
+using chdScoring.App.UI.Extensions;
 using chdScoring.App.UI.Interfaces;
 using chdScoring.App.WPF.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Markup;
 using System.Windows;
-using Microsoft.Extensions.Hosting;
+using System.Windows.Markup;
 
 namespace chdScoring.App.WPF.Extensions
 {
@@ -19,10 +20,14 @@ namespace chdScoring.App.WPF.Extensions
     {
         public static IServiceCollection AddChdScoringApp(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddKeyedSingleton(SettingConstants.AvailableLanguages, Dict());
+
             services.AddChdScoringAppUI<VibrationHelper, UpdateService, SettingManager, BatteryService, TTSService>(configuration);
 
             services.AddSingleton<INotificationManagerService, NotificationManagerService>();
             return services;
         }
+
+        private static Task<Dictionary<string, string>> Dict() => Task.FromResult(new Dictionary<string, string>());
     }
 }
