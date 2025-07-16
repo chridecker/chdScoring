ae35aaa8e5d512fc177903cf1dc91733bfc5d314
Christoph Decker <C.Decker@metalldeutsch.com>
2025-07-16 13:47:59 +0200

    WCF Setting

--- src/chdScoring.App.WPF/Services/SettingManager.cs
+++ src/chdScoring.App.WPF/Services/SettingManager.cs
@@ -38,6 +38,6 @@ namespace chdScoring.App.WPF.Services
             return Task.CompletedTask;
         }
 
-        protected override bool _isiOS()=> true;
+        protected override bool _isiOS()=> false;
     }
 }
