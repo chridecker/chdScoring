0b43d0fc3998eb18705647124fe9d1c418511655
Christoph Decker <C.Decker@metalldeutsch.com>
2025-07-26 05:28:32 +0200

    round

--- src/chdScoring.Web/Models/RoundResultContainer.cs
+++ src/chdScoring.Web/Models/RoundResultContainer.cs
@@ -5,5 +5,6 @@ namespace chdScoring.Web.Models
     public class RoundResultContainer
     {
         public List<RoundResultDto> Dtos { get; set; } = new List<RoundResultDto>();
+        public string GetImageUrl(ImageDto dto) => $"data:{dto.Type};base64,{Convert.ToBase64String(dto.Data)}";
     }
 }
