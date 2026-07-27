1b9e0426b5162d4b328056e6b9221af2cdde2824
Christoph Decker <Christoph.Decker@binderholz.com>
2026-07-27 10:11:02 +0200

    Score Segenmenbts

--- src/chdScoring.Contracts/Dtos/ScoreImportDto.cs
+++ src/chdScoring.Contracts/Dtos/ScoreImportDto.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace chdScoring.Contracts.Dtos
 {
-    public class RoundImportDto
+    public class ScoreImportDto
     {
         public int Figure { get; set; }
         public decimal Value { get; set; }
+        public decimal Intra { get; set; }
+        public decimal Inter { get; set; }
+        public decimal Box { get; set; }
     }
 }
